55a39efe5b1b6b4dde72b7b668a234c74332912c
Marcel Haerry <haerry@puzzle.ch>
2008-04-29 16:21:34 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# sudo module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "sudo": }
+class sudo {}
