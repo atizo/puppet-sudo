22b03cbe04c33ea359c96217e9c904675ead8383
Marcel Haerry <haerry@puzzle.ch>
2008-04-29 16:29:31 +0200

    added package definition

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,19 @@
 # sudo module
 # Puzzle ITC - haerry+puppet(at)puzzle.ch
 # GPLv3
+# manages sudo 
 #######################################
 
 
 # modules_dir { "sudo": }
-class sudo {}
+class sudo {
+    case $operatingsystem {
+        default: { include sudo::base }
+    }
+}
+
+class sudo::base {
+    package{sudo:
+        ensure => installed,
+    }
+}
