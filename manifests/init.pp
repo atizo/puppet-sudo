8b1bc8792777c8bac4655cc69809766826d974ae
Simon Josi <sjosi@pw-sjosi.puzzle.itc>
2008-05-29 18:39:42 +0200

    added puzzle copyright notice

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,18 @@
-#######################################
+#
 # sudo module
-# Puzzle ITC - haerry+puppet(at)puzzle.ch
-# GPLv3
-# manages sudo 
-#######################################
-
+#
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
 
 # modules_dir { "sudo": }
+
 class sudo {
     case $operatingsystem {
         default: { include sudo::base }
